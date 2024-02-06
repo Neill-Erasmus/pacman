0d36f58e3cfcbec0638beba158153ab4a58fd79e
Neill Erasmus <neill.erasmus20@gmail.com>
2024-02-06 14:11:31 +0200

    Updated environment class.

--- environment.py
+++ environment.py
@@ -1,14 +1,14 @@
-import gymnasium as gym
+import gym
 
-class Environment():
+class Environment:
     """
     Environment class wraps the gym environment for the RL agent.
 
     Attributes:
-        env: The gym environment instance.
-        state_shape: The shape of the state space.
-        state_size: The size of the state space.
-        number_actions: The number of actions in the action space.
+        env (gym.Env): The gym environment instance.
+        state_shape (tuple): The shape of the state space.
+        state_size (int): The size of the state space.
+        number_actions (int): The number of actions in the action space.
     """
 
     def __init__(self) -> None:
@@ -16,18 +16,18 @@ class Environment():
         Initializes the Environment object with a specific gym environment ('MsPacmanDeterministic-v0').
         """
 
-        self.env = gym.make('MsPacmanDeterministic-v0', full_action_space=False)
-        self.state_shape = self.env.observation_space.shape
-        self.state_size = self.state_shape[0]  # type: ignore
-        self.number_actions = self.env.action_space.n  # type: ignore
+        self.env            : gym.Env = gym.make('MsPacmanDeterministic-v0', full_action_space=False)
+        self.state_shape    : tuple   = self.env.observation_space.shape #type: ignore
+        self.state_size     : int     = self.state_shape[0]              #type: ignore
+        self.number_actions : int     = self.env.action_space.n          #type: ignore
         print(f'State Shape: {self.state_shape}\nState Size: {self.state_size}\nNumber of Actions: {self.number_actions}')
 
-    def step(self, action):
+    def step(self, action : int) -> tuple:
         """
         Performs a step in the environment.
 
         Args:
-            action: The action to take.
+            action (int): The action to take.
 
         Returns:
             Tuple containing state, reward, done, and additional info.
@@ -35,7 +35,7 @@ class Environment():
 
         return self.env.step(action)
 
-    def reset(self):
+    def reset(self) -> object:
         """
         Resets the environment.
 
